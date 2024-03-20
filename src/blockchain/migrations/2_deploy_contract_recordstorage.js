531cdb6337c3ba7702036c7b3fefd2ba8b7a99c0
CybrToad <34751856+cybrtoad@users.noreply.github.com>
2024-03-20 16:37:40 -0600

    Delete src/blockchain directory

--- src/blockchain/migrations/2_deploy_contract_recordstorage.js
+++
@@ -1,9 +0,0 @@
-// CredHub - Credentials and Identity Manager
-// @team Tamara Linse, Megan Steeves, Selma Samet, and Kellen Mentock
-// @version 1.0
-// @date 3.15.2024
-
-var RecordStorage = artifacts.require("RecordStorage");
-  module.exports = function(deployer) {
-  deployer.deploy(RecordStorage);
-};
